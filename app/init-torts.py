1574795d071af3041a8028055fb8270205a5b7cb
dev <dev@penguinbeam>
2018-07-01 18:34:59 +0100

    Fix python3 open file as binary issue

--- app/init-torts.py
+++ app/init-torts.py
@@ -6,11 +6,11 @@ tortmapjsonURL = os.environ["TORTMAPURL"]
 
 from urllib.request import urlopen
 
-data = urlopen(tortmapjsonURL).read(20000)
+data = urlopen(tortmapjsonURL).read(20000).decode('utf-8')
 data = data.split("\n")
 data = str(data)
 
 #for line in data:
-jsonmap = open("app/tort2devicemap.json", "w", 1)
+jsonmap = open("app/tort2devicemap.json", "a", 1)
 jsonmap.write(data)
 jsonmap.close()
